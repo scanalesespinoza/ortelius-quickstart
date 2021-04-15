f9babe539f36a21c9232a5ff8bbcea717d992291
Sergio Canales <scanales@redhat.com>
2021-04-15 07:58:27 -0400

    Adding istio minimal setup, redirect for ortelius home and tools to deploy mock server

--- add-istio-into-minikube.sh
+++ add-istio-into-minikube.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Installing Istio"
+sh scripts/istio-setup-minimal.sh
+kubectl label namespace default istio-injection=enabled --overwrite
+kubectl get namespace -L istio-injection
 if kubectl apply -f ortelius-istio/ ; then
      echo "Your Istio routing is ready!"
 else
